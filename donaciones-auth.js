1680b22689004f8dded704b8172a20e7c0668993
Jesus11-ops <143579983+Jesus11-ops@users.noreply.github.com>
2026-02-09 23:47:59 -0500

    Update donaciones-auth.js

--- donaciones-auth.js
+++ donaciones-auth.js
@@ -32,7 +32,7 @@ window.login = function () {
   console.log('Intentando login con', email);
   signInWithEmailAndPassword(auth, email.trim(), password)
     .then(() => {
-      window.location.href = "donaciones-dashboard.html";
+      window.location.href = "donaciones-FOSU2.html";
     })
     .catch((err) => {
       console.error('Login error', err);
